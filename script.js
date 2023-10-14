cd9424819b67ca8f9f217eda1e60650e54d8fe31
sashaij <o.zagorodniuk@outlook.com>
2023-10-14 16:17:20 +0300

    Add beginning styles for validation indication, select inputs in js file

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const userFirstName = document.getElementById('first-name');
+const userLastName = document.getElementById('last-name');
+const userEmail = document.getElementById('email');
+const userPhone = document.getElementById('phone-number');
+const userPassword = document.getElementById('password');
+const userConfPassword = document.getElementById('confirm-password');
